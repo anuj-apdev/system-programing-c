c5289eae0f890baf1bb3fa20edb4d36895078b3b
Anuj Pandey <anuj.iitbhu@live.com>
2021-04-21 16:37:20 +0530

    Intial merge

---
+++ IPC-pipe/say.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        printf("i'm saying %d\n", i);
+        fflush(stdout);
+        if (i != 9) {
+            sleep(1);
+        }
+    }
+    return 0;
+}
